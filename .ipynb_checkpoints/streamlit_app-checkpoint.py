eeeae80e7245f2418baf3c057a9a66807d2ac188
ayoolaaa <92917007+ayoolaaa@users.noreply.github.com>
2021-12-16 14:39:26 +0000

    Update streamlit_app-checkpoint.py

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -13,22 +13,22 @@ car_input = {'make':'xyz',
 'fuel':'xyz',
 'owners':'xyz',
 'body':'xyz',
-'ULEZ':'xyz',
-'Engine':'xyz',
+'ulez':'xyz',
+'engine':'xyz',
 }
 
 car_makes = ['Audi', 'Ford', 'Mercedes', 'Nissan', 'Vauxhall', 'Volkswagen', '']
+car_input['make'] = st.selectbox('Please select the car make:', car_makes)
 
 audi_models = ['A1', 'A3', 'A4', 'A5', 'A6', 'Q2', 'Q3', 'Q5', 'Q7', 'S3', 'TT']
 ford_models = ['B-Max', 'C-Max', 'Edge', 'Fiesta', 'Focus', 'Fusion', 'Galaxy', 'Grand C-Max', 'KA', 'KA Plus',
               'Kuga', 'Mondeo', 'Mustang', 'Puma', 'Ranger', 'S-Max', 'Transit', 'Transit Custom']
-mercedes_models = []
-nissan_models = []
-vauxhall_models = []
-volkswagen_models = []
+mercedes_models = ['A Class', 'B Class', 'C Class', 'CLA Class', 'E Class', 'GLA Class', 'GLC Class', 'GLE Class',
+                   'S Class']
+nissan_models = ['370Z', 'Elgrand', 'Juke', 'Micra', 'Navara', 'Pixo', 'Qashqai', 'X-trail']
+vauxhall_models = ['Adam', 'Astra', 'Corsa', 'Crossland X', 'Grandland X', 'Insignia', 'Mokka', 'Non-Franchise C']
+volkswagen_models = ['Beetle', 'Golf', 'Passat', 'Polo', 'Scirocco', 'T-Roc', 'Tiguan', 'Up']
 _models = []
-
-car_input['make'] = st.selectbox('Please select the car make:', car_makes)
 
 if 'Audi' in car_input['make']:
     car_input['model'] = st.selectbox('Please select the car model:', audi_models)
@@ -50,3 +50,25 @@ elif 'Volkswagen' in car_input['make']:
     
 else :
     car_input['model'] = st.selectbox('Please select the car model:', _models)
+    
+car_year = ['Before 2005','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016',
+            '2017','2018','2019','2020', '2021']
+car_input['year'] = st.selectbox('Please select the car year:', car_year)
+    
+car_input['mileage'] = st.text_input('Please enter the car mileage:')
+
+car_transmission = ['Automatic', 'Manual']
+car_input['transmission'] = st.radio('What transmission does the car have?', car_transmission)
+
+car_fuel = ['Petrol', 'Diesel']
+car_input['fuel'] = st.radio('What fuel does the car use?', car_fuel)
+
+car_input['engine'] = st.slider('What is the car engine size? (L)', min_value=0, max_value=9, step=0.1)
+
+car_input['owners'] = st.slider('How many owners has the car had?', min_value=0, max_value=100, step=1)
+    
+ulez = ['Yes', 'No']
+car_input['ulez'] = st.radio('Is the car ULEZ compliant?', ulez)
+
+st.button("Price", key=None, help=None, on_click=None, args=None, kwargs=None)
+
