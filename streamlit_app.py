730bfc504af36fe5f08ed4c2081f3d302c6ecefc
ayoolaaa <92917007+ayoolaaa@users.noreply.github.com>
2021-12-16 11:03:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,16 @@ car_input = {'make':'xyz',
 'Engine':'xyz',
 }
 
-car_makes = ['Audi', 'Ford', 'Mercedes', 'Nissan', 'Vauxhall', 'Volkswagen', ]
+car_makes = ['Audi', 'Ford', 'Mercedes', 'Nissan', 'Vauxhall', 'Volkswagen', '']
+audi_models = ['A1', 'A3', 'A4', 'A5', 'A6', 'Q2', 'Q3', 'Q5', 'Q7', 'S3', 'TT']
+ford_models = ['C-Max', 'Edge', 'Fiesta', 'Focus', 'Fusion', 'Galaxy', 'Grand C-Max', 'KA Plus','KA',
+              'Kuga', 'Mondeo', 'Puma', 'Mustang', 'Ranger', 'S-Max', 'Transit Custom', 'Transit', 'B-Max']
 
 car_input['make'] = st.selectbox('Please select the car make:', car_makes)
+
+if 'Audi' in car_input['make']:
+    car_input['model'] = st.selectbox('Please select the car model:', audi_models)
+    
+if 'Ford' in car_input['make']:
+    car_input['model'] = st.selectbox('Please select the car model:', ford_models)
+    
